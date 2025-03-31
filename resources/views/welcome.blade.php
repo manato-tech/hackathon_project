321ea0511e3e1bdc46d14ecb46b0f052faa1de9a
tiromo <tomori2003@gmail.com>
2025-03-31 19:16:11 +0900

    ようこそをwelcomeに変更(テスト)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body class="flex items-center justify-center min-h-screen bg-gray-100">
     <div class="text-center">
-        <h1 class="text-4xl font-bold mb-6">ようこそ</h1>
+        <h1 class="text-4xl font-bold mb-6">welcome</h1>
         <div class="space-x-4">
             <a href="{{ route('login') }}" class="px-6 py-3 bg-blue-500 text-white rounded-lg text-lg">ログイン</a>
             <a href="{{ route('register') }}" class="px-6 py-3 bg-green-500 text-white rounded-lg text-lg">登録</a>
