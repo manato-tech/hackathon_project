9705f8771716b027bfe264fe6ca2c49a47c6b6e8
tiromo <tomori2003@gmail.com>
2025-04-01 11:09:45 +0900

    ヘッダー、フッターを共通規格で表示できるようにした

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,39 +1,12 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ToDoList</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
+<x-guest-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Top Page
+        </h2>
+    </x-slot>
 
-<body class="flex flex-col h-screen">
-    <header class="absolute inset-x-0 top-0 h-16 bg-slate-800 text-3xl flex justify-between items-center">
-    <div class="text-white px-5">    
-            ToDoList
-        </div>
-        <div class="flex space-x-5 px-5">
-            <div class="text-white">
-                <a href="/">トップページ</a>
-            </div>
-
-            <div class="text-white">
-                <a href="{{ route('todo.create') }}" class="create-form">フォーム作成</a>
-            </div>
-
-            <div class="text-white">
-                <a href="{{ route('dashboard') }}" class="login">ログイン</a>
-            </div>
-            <div class="text-white">
-                <a href="{{ route('register') }}" class="register">アカウント登録</a>
-        
-            </div>    
-    
-        </div>
-    </header>
-
-    <main class="flex items-center justify-center min-h-screen bg-gray-100">
-        <div class="text-center space-y-4">
+    <main class="flex justify-center my-auto bg-white max-w-max">
+        <div class="text-center space-y-6">
             <h1 class="text-4xl font-bold mb-6">Aim Higher</h1>
             <intro>少しでもより良い生活の実現のため、あなたの仕事や家事の管理を手助けします。</intro>
             <div class="space-x-4">
@@ -43,20 +16,4 @@
         </div>
     </main>
 
-    <footer>
-        <div class="absolute inset-x-0 h-16 bg-slate-300 text-3xl flex items-center justify-center border-t">
-            <div class="flex space-x-5">
-                <div>    
-                    ToDoList
-                </div>
-                <div class="text-blue-600">
-                    <a href="contact.html" class="contact">お問い合わせ</a>
-                </div>
-            </div>
-            <div>    
-                はこちら
-            </div>
-    </footer>
-</body>
-
-</html>
+</x-guest-layout>
