292b139bb1a6fb5e7a1170fdafa3045f9fe95e48
tiromo <tomori2003@gmail.com>
2025-04-02 18:38:32 +0900

    リストのページ作成

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,8 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect('toppage');
+        // ->intended(route('toppage', absolute: false));
     }
 
     /**
