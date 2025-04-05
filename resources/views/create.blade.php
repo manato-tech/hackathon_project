e8b24edeee72e6dccc33f252c6176c611c704926
tiromo <tomori2003@gmail.com>
2025-04-05 18:32:07 +0900

    締切関係の整備

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -12,11 +12,6 @@
                 @csrf
 
                 <div class="w-full flex flex-col">
-                <label for="deadline" class="font-semibold mt-4">締切:</label>
-                <input type="datetime-local" name="deadline" id="deadline" class="w-full py-2 px-3 border border-gray-300 rounded-md">
-                </div>
-
-                <div class="w-full flex flex-col">
                     <label for="title" class="font-semibold mt-4">件名</label>
                     <input type="text" name="title" class="w-full py-2 px-3 border border-gray-300 rounded-md" id="title">
                 </div>
@@ -24,6 +19,11 @@
                 <div class="w-full flex flex-col">
                     <label for="body" class="font-semibold mt-4">本文</label>
                     <textarea name="body" class="w-full py-2 px-3 border border-gray-300 rounded-md" id="body" cols="30" rows="5" ></textarea>
+                </div>
+
+                <div class="w-full flex flex-col">
+                    <label for="deadline" class="font-semibold mt-4">締切<a for="deadline" class="text-xs mt-4">（デフォルトは現在日時）</a></label>
+                    <input type="datetime-local" name="deadline" id="deadline" class="w-full py-2 px-3 border border-gray-300 rounded-md" value="{{ old('deadline', now()->format('Y-m-d\TH:i')) }}">
                 </div>
 
                 <div class="my-4">
